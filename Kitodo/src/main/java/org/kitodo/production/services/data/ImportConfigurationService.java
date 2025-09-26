acd9df5f52a2b778c7849393061ca69ce8f03edd
Arved Solth <solth@effective-webwork.de>
2025-09-26 17:03:29 +0200

    Fix Checkstyle issues

--- Kitodo/src/main/java/org/kitodo/production/services/data/ImportConfigurationService.java
+++ Kitodo/src/main/java/org/kitodo/production/services/data/ImportConfigurationService.java
@@ -22,6 +22,8 @@ import java.util.Map;
 import java.util.Objects;
 import java.util.stream.Collectors;
 
+import javax.xml.parsers.ParserConfigurationException;
+
 import org.kitodo.api.externaldatamanagement.ImportConfigurationType;
 import org.kitodo.api.externaldatamanagement.SearchInterfaceType;
 import org.kitodo.data.database.beans.ImportConfiguration;
@@ -36,8 +38,6 @@ import org.primefaces.model.SortOrder;
 import org.w3c.dom.Document;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
-
-import javax.xml.parsers.ParserConfigurationException;
 
 public class ImportConfigurationService extends BaseBeanService<ImportConfiguration, ImportConfigurationDAO> {
 
@@ -160,7 +160,8 @@ public class ImportConfigurationService extends BaseBeanService<ImportConfigurat
      * @throws ParserConfigurationException when parsing the XML response from the SRU explain operation fails
      * @throws SAXException when parsing the XML response from the SRU explain operation fails
      */
-    public static List<String> getAvailableSruRecordSchemata(ImportConfiguration config) throws IOException, ParserConfigurationException, SAXException {
+    public static List<String> getAvailableSruRecordSchemata(ImportConfiguration config) throws IOException, ParserConfigurationException,
+            SAXException {
         List<String> recordSchemata = new ArrayList<>();
         URL explainUrl = getSruExplainUrl(config);
         Document explainDocument = XMLUtils.loadXmlDocumentByUrl(explainUrl);
